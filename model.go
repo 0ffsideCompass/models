a6433bc4b232a572ec699d4d4b29c1378e193d04
Iordanis Paschalidis <iordanispaschalidis@Iordaniss-Mac-mini.local>
2022-05-23 12:39:25 +0100

    adding go-dasta models

---
+++ model.go
@@ -0,0 +1,45 @@
+package models
+
+import "time"
+
+// ClubSeasonDataJSONRequest represents the request for the endpoint /club/season
+type ClubSeasonDataJSONRequest struct {
+	League string    `json:"league"`
+	Data   []Fixture `json:"data"`
+}
+
+// Fixture represents a game between two teams
+type Fixture struct {
+	TeamOne string `json:"teamOne"`
+	TeamTwo string `json:"teamTwo"`
+}
+
+// ClubSeasonDataJSONResponse response for endpoint /club/season
+type ClubSeasonDataJSONResponse struct {
+	HistoricalData []HistoricalData `json:"data"`
+}
+
+// HistoricalData respresnt the hitorical games between two games
+type HistoricalData struct {
+	TeamsID  string        `json:"id"`
+	TeamOne  string        `json:"teamOne"`
+	TeamTwo  string        `json:"teamTwo"`
+	Fixtures []ClubFixture `json:"fixtures"`
+}
+
+// ClubFixture represents the details of a game between two teams
+type ClubFixture struct {
+	Div              string    `json:"div"`
+	Date             time.Time `json:"date"`
+	HomeTeam         string    `json:"homeTeam"`
+	AwayTeam         string    `json:"awayTeam"`
+	FullTimeHomeGoal string    `json:"fullTimeHomeGoal"`
+	FullTimeAwayGoal string    `json:"fullTimeAwayGoal"`
+	Winner           string    `json:"winner"`
+	HalfTimeHomeGoal string    `json:"halfTimeHomeGoal"`
+	HalfTimeAwayGoal string    `json:"halfTimeAwayGoal"`
+	HalfTimeWinner   string    `json:"halfTimeWinner"`
+	Attendance       string    `json:"attendance"`
+	Referee          string    `json:"referee"`
+	Season           int32     `json:"season"`
+}
