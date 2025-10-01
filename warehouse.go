69e96345efd42854ca2a4d1dabe01c8837872f38
Iordanis PAschaliidiis <iordanis.paschalidis@yahoo.com>
2025-10-01 09:44:21 +0100

    tidying up

--- warehouse.go
+++ warehouse.go
@@ -1,57 +1,20 @@
 package models
 
-import "time"
-
-// DataWarehouseArticle represents the domain model for an article
-type DataWarehouseArticle struct {
-	ID          string    `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	URL         string    `json:"url"`
-	Tags        []string  `json:"tags"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-}
-
 // DataWarehouseCreateArticleRequest represents the request to create an article
 type DataWarehouseCreateArticleRequest struct {
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	URL         string   `json:"url"`
-	Tags        []string `json:"tags"`
-}
-
-// DataWarehouseUpdateArticleRequest represents the request to update an article
-type DataWarehouseUpdateArticleRequest struct {
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Tags        []string `json:"tags"`
-}
-
-// DataWarehouseArticleResponse represents the API response for an article
-type DataWarehouseArticleResponse struct {
-	Article *DataWarehouseArticle `json:"article,omitempty"`
-	Error   string                `json:"error,omitempty"`
+	ExternalID string   `json:"external_id"`
+	Title      string   `json:"title"`
+	URL        string   `json:"url"`
+	Tags       []string `json:"tags"`
 }
 
 // DataWarehouseArticlesResponse represents the API response for multiple articles
 type DataWarehouseArticlesResponse struct {
-	Articles []DataWarehouseArticle `json:"articles"`
-	Total    int                    `json:"total"`
-	Page     int                    `json:"page,omitempty"`
-	Limit    int                    `json:"limit,omitempty"`
-	Error    string                 `json:"error,omitempty"`
-}
-
-// DataWarehousePodcast represents the domain model for a podcast
-type DataWarehousePodcast struct {
-	ID          string    `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	URL         string    `json:"url"`
-	Tags        []string  `json:"tags"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	Articles []Article `json:"articles"`
+	Total    int       `json:"total"`
+	Page     int       `json:"page,omitempty"`
+	Limit    int       `json:"limit,omitempty"`
+	Error    string    `json:"error,omitempty"`
 }
 
 // DataWarehouseCreatePodcastRequest represents the request to create a podcast
@@ -62,24 +25,11 @@ type DataWarehouseCreatePodcastRequest struct {
 	Tags        []string `json:"tags"`
 }
 
-// DataWarehouseUpdatePodcastRequest represents the request to update a podcast
-type DataWarehouseUpdatePodcastRequest struct {
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Tags        []string `json:"tags"`
-}
-
-// DataWarehousePodcastResponse represents the API response for a podcast
-type DataWarehousePodcastResponse struct {
-	Podcast *DataWarehousePodcast `json:"podcast,omitempty"`
-	Error   string                `json:"error,omitempty"`
-}
-
 // DataWarehousePodcastsResponse represents the API response for multiple podcasts
 type DataWarehousePodcastsResponse struct {
-	Podcasts []DataWarehousePodcast `json:"podcasts"`
-	Total    int                    `json:"total"`
-	Page     int                    `json:"page,omitempty"`
-	Limit    int                    `json:"limit,omitempty"`
-	Error    string                 `json:"error,omitempty"`
+	Podcasts []Podcast `json:"podcasts"`
+	Total    int       `json:"total"`
+	Page     int       `json:"page,omitempty"`
+	Limit    int       `json:"limit,omitempty"`
+	Error    string    `json:"error,omitempty"`
 }
